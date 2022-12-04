2f957c24f6ea6448f2c393304216074c1a7ef090
Данила Романович <voloshenko.inc@gmail.com>
2022-12-05 01:55:39 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <app-navbar/>
+  <app-main/>
+  <app-gallery/>
+  <app-footer/>
+</template>
+
+<script>
+import AppMain from './components/AppMain.vue'
+import AppFooter from './components/AppFooter.vue'
+import AppGallery from './components/AppGallery.vue'
+import AppNavbar from './components/AppNavbar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AppNavbar,
+    AppMain,
+    AppGallery,
+    AppFooter
+  }
+
+}
+</script>
+
+<style lang="scss">
+</style>
